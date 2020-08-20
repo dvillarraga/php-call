a321c1d4a1576a78612b93631e85633c128dedde
Oscar Daniel Villarraga Castro <dvillarraga@gmail.com>
2020-08-20 11:10:09 -0500

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,3 +1,24 @@
-<?php 
+<html>
+<head>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+</head>
+<body>
+<script>
+$( document ).ready(function() {
+  $.ajax({
+    url: "http://phpservice-service",
+    dataType: "json"
+  }).done(function(result) {
+    $("#result").html(result.message);
+  });
+});
 
-echo '<h1>HELLO AND WELCOME</h1>';
+
+
+</script>
+  <?php echo '<h1>HELLO AND WELCOME</h1>';?>
+  <p> The result is:</p>
+  <p id="result"></p>
+</body>
+
+</html>
